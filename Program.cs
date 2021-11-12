94c3ff76c23a817db09f2f7c0b77d1d3bb63bbeb
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-11-12 12:33:10 +0100

    Triangles move independently

--- Program.cs
+++ Program.cs
@@ -10,24 +10,29 @@ namespace SharpEngine
 {
     class Program
     {
-        static Triangle triangle = new Triangle
-        (
+        static Triangle triangle = new(
             new Vertex[] 
             {
-                new Vertex(new Vector(.2f, .2f), Color.Red),
-                new Vertex(new Vector(.8f, .6f), Color.Green),
-                new Vertex(new Vector(.8f, .1f), Color.Blue),
-                
-                new Vertex(new Vector(.2f, .0f), Color.Red),
-                new Vertex(new Vector(.5f, .0f), Color.Green),
-                new Vertex(new Vector(.5f, .1f), Color.Blue)
+                new (new Vector(0.5f, 0.5f), Color.Red),
+                new (new Vector(.2f, .9f), Color.Green),
+                new (new Vector(.8f, .9f), Color.Blue),
             }
         );
-        static void Main(string[] args)
+
+        private static Triangle triangle2 = new(
+            new Vertex[]
+            {
+                new (new Vector(-0.5f, -0.5f), Color.Red),
+                new (new Vector(.0f, .5f), Color.Green),
+                new (new Vector(0.5f, -0.5f), Color.Blue)
+            }
+        );
+
+        private static Triangle[] _triangles = {triangle, triangle2};
+        static void Main()
         {
             //initialize and configure
             var window = CreateWindow();
-            LoadTriangleIntoBuffer();
             CreateShaderProgram();
             
             var direction = new Vector(0.0002f, 0.0002f);
@@ -35,38 +40,27 @@ namespace SharpEngine
             //Engine rendering loop
             while (!Glfw.WindowShouldClose(window))
             {
-                Glfw.PollEvents(); //reacts to window changes (position etc.)
-                ClearScreen();
-                Render(window);
-                triangle.Scale(multiplier);
+                foreach (var triangle in _triangles)
+                {
+                    Glfw.PollEvents(); //reacts to window changes (position etc.)
+                    ClearScreen();
+                    Render(window);
+                    triangle.Scale(multiplier);
+                    triangle2.Scale(multiplier);
+
+                    multiplier = triangle.CurrentScalar(multiplier);
+                    direction = triangle.MoveDirection(direction);
+                    multiplier = triangle2.CurrentScalar(multiplier);
+                    direction = triangle2.MoveDirection(direction);
+                }
                 
-                // 2. Keep track of the Scale, so we can reverse it
-                if (triangle.CurrentScale <= 0.5f) {
-                    multiplier = 1.001f;
-                }
-                if (triangle.CurrentScale >= 1f) {
-                    multiplier = 0.999f;
-                }
-                //3. Move Triangle by its direction
-               triangle.Move(direction);
-               
-                // 4. Check the X-Bounds of the Screen
-                if(triangle.GetMaxBounds().x >= 1 && direction.x > 0  || triangle.GetMinBounds().x <= -1 && direction.x < 0)
-                {
-                    direction.x *= -1;
-                }
-            
-                //5. Same as 4 but for y
-                if(triangle.GetMaxBounds().y >= 1 && direction.y > 0  || triangle.GetMinBounds().y <= -1 && direction.y < 0)
-                {
-                    direction.y *= -1;
-                }
             }
         }
-    
+        
         private static void Render(Window window)
         {
-            triangle.Render();
+            triangle.Render(triangle.LoadTriangleIntoBuffer());
+            triangle2.Render(triangle2.LoadTriangleIntoBuffer());
             Glfw.SwapBuffers(window);
             //glFlush();
         }
@@ -94,17 +88,7 @@ namespace SharpEngine
             glLinkProgram(program);
             glUseProgram(program);
         }
-        private static unsafe void LoadTriangleIntoBuffer()
-        {
-            var vertexArray = glGenVertexArray();
-            var vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
-            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
-            glEnableVertexAttribArray(0);
-            glEnableVertexAttribArray(1);
-        }
+        
         
         private static Window CreateWindow()
         {
--- Triangle.cs
+++ Triangle.cs
@@ -1,4 +1,7 @@
+using System.Runtime.InteropServices;
 using OpenGL;
+using static OpenGL.Gl;
+
 
 namespace SharpEngine
 {
@@ -9,6 +12,7 @@ namespace SharpEngine
         {
             this.vertices = vertices;
             CurrentScale = 1f;
+            Render(LoadTriangleIntoBuffer());
         }
 
         public float CurrentScale { get; private set;}
@@ -56,13 +60,61 @@ namespace SharpEngine
                 vertices[i].position += direction;
             }
         }
+        public Vector MoveDirection(Vector direction)
+        {
+            //3. Move Triangle by its direction
+            Move(direction);
+               
+            // 4. Check the X-Bounds of the Screen
+            if(GetMaxBounds().x >= 1 && direction.x > 0  || GetMinBounds().x <= -1 && direction.x < 0)
+            {
+                direction.x *= -1;
+            }
+            
+            //5. Same as 4 but for y
+            if(GetMaxBounds().y >= 1 && direction.y > 0  || GetMinBounds().y <= -1 && direction.y < 0)
+            {
+                direction.y *= -1;
+            }
 
-        public unsafe void Render() 
+            return direction;
+        }
+        public float CurrentScalar(float multiplier)
+        {
+            // 2. Keep track of the Scale, so we can reverse it
+            if (CurrentScale <= 0.5f) 
+            {
+                multiplier = 1.001f;
+            }
+            if (CurrentScale >= 1f) 
+            {
+                multiplier = 0.999f;
+            }
+
+            return multiplier;
+        }
+
+        public unsafe void Render(uint vertexArray) 
         {
             fixed (Vertex* vertex = &vertices[0]) {
-                Gl.glBufferData(Gl.GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, Gl.GL_DYNAMIC_DRAW);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_DYNAMIC_DRAW);
             }
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, vertices.Length);
+            glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
+            glBindVertexArray(vertexArray);
+        }
+        //2. Fix your Code to make it work with two separate Triangle instances.
+        //Hint: You need to save the return value of glGenVertexArray and then always call glBindVertexArray
+        //before Rendering the Triangle in its Render-Method. Important, you need to do that as the first thing in the Render-Method.
+        public unsafe uint LoadTriangleIntoBuffer()
+        {
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
+            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
+            glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
+            return vertexArray;
         }
     }
 }
