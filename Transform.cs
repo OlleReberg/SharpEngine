5fa49fb422300eaab43144cd3398cae9c4b88491
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-11-16 10:59:13 +0100

    Matrix resurrections

--- Transform.cs
+++ Transform.cs
@@ -7,26 +7,25 @@ namespace SharpEngine {
     public class Transform {
         public Vector CurrentScale { get; private set; }
         public Vector Position { get; private set; }
-        public Matrix Matrix => Matrix.Translation(Position) * Matrix.Scale(CurrentScale);
+        public Vector Rotation { get; private set; }
+        public Matrix Matrix => Matrix.Translation(Position) * Matrix.Rotation(Rotation) * Matrix.Scale(CurrentScale);
 
-        public Transform() 
-        {
-            CurrentScale = new Vector(1, 1, 1);
+        public Transform() {
+            this.CurrentScale = new Vector(1, 1, 1);
         }
-
-        public void Scale(float multiplier) 
-        {
+		
+        public void Scale(float multiplier) {
             CurrentScale *= multiplier;
         }
 
-        public void Move(Vector direction) 
-        {
+        public void Move(Vector direction) {
             this.Position += direction;
         }
 
-        public void Rotate(float rotation) 
-        {
-            
+        public void Rotate(float zAngle) {
+            var rotation = this.Rotation;
+            rotation.z += zAngle;
+            this.Rotation = rotation;
         }
     }
 }
