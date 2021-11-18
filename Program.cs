4a1059f99bbced19127a03c4b0fbc66b8b75f299
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-11-18 11:02:51 +0100

    Math I don't fully understand making bouncy bouncy done right

--- Program.cs
+++ Program.cs
@@ -1,113 +1,51 @@
 ﻿using System;
-using GLFW;
-
-using System;
+using System.Numerics;
 using GLFW;
 
 namespace SharpEngine
 {
-    class Program
+    class Program 
     {
-        static float Lerp(float from, float to, float t)
+        static float Lerp(float from, float to, float t) 
         {
             return from + (to - from) * t;
         }
-
-        static float GetRandomFloat(Random random, float min = 0, float max = 1)
+        static float GetRandomFloat(Random random, float min = 0, float max = 1) 
         {
-            return Lerp(min, max, (float) random.Next() / int.MaxValue);
+            return Lerp(min, max, (float)random.Next() / int.MaxValue);
         }
-
-        static void Main(string[] args)
+        
+        static void Main(string[] args) 
         {
+            var random = new Random();
             var window = new Window();
             var material = new Material("shaders/world-position-color.vert", "shaders/vertex-color.frag");
             var scene = new Scene();
+            var physics = new Physics(scene);
             window.Load(scene);
 
-            var triangle = new Triangle(material);
-            triangle.Transform.CurrentScale = new Vector(0.5f, 1f, 1f);
-            scene.Add(triangle);
-
-            var rectangle = new Rectangle(material);
-            rectangle.Transform.CurrentScale = new Vector(1f, 0.5f, 1f);
-            rectangle.Transform.Position = new Vector(0f, -0.5f, 1f);
-            scene.Add(rectangle);
-
-            var circle = new Circle(material);
-            circle.Transform.CurrentScale = new Vector(3f, 3f, 3f);
-            circle.Transform.Position = new Vector(-0.5f, 0.5f, 1f);
-            scene.Add(circle);
-
-            var ground = new Rectangle(material);
-            ground.Transform.CurrentScale = new Vector(10f, 1f, 1f);
-            ground.Transform.Position = new Vector(0f, -1f);
-
-            scene.Add(ground);
-
-            // engine rendering loop
-            const int fixedStepNumberPerSecond = 30;
-            const float fixedDeltaTime = 1.0f / fixedStepNumberPerSecond;
-            const float movementSpeed = 0.5f;
-            double previousFixedStep = 0.0;
-            while (window.IsOpen())
+            for (var i = 0; i < 30; i++) 
             {
-                while (Glfw.Time > previousFixedStep + fixedDeltaTime)
+                var circle = new Circle(material);
+                var radius = GetRandomFloat(random, 0.3f);
+                circle.Transform.CurrentScale = new Vector(radius, radius, 1f);
+                circle.Transform.Position = new Vector(GetRandomFloat(random, -1f), GetRandomFloat(random, -1), 0f);
+                circle.velocity = -circle.Transform.Position.Normalize() * GetRandomFloat(random, 0.15f, 0.3f);
+                circle.Mass = MathF.PI * radius * radius;
+                scene.Add(circle);
+            }
+
+            const int fixedStepNumberPerSecond = 60;
+            const float fixedDeltaTime = 1.0f / fixedStepNumberPerSecond;
+            const int maxStepsPerFrame = 5;
+            var previousFixedStep = 0.0;
+            while (window.IsOpen()) 
+            {
+                var stepCount = 0;
+                while (Glfw.Time > previousFixedStep + fixedDeltaTime && stepCount++ < maxStepsPerFrame) 
                 {
                     previousFixedStep += fixedDeltaTime;
-                    var walkDirection = new Vector();
-
-                    if (window.GetKey(Keys.W))
-                    {
-                        walkDirection += triangle.Transform.Forward;
-                    }
-
-                    if (window.GetKey(Keys.A))
-                    {
-                        walkDirection += triangle.Transform.Left;
-                    }
-
-                    if (window.GetKey(Keys.S))
-                    {
-                        walkDirection += triangle.Transform.Backward;
-                    }
-
-                    if (window.GetKey(Keys.D))
-                    {
-                        walkDirection += triangle.Transform.Right;
-                    }
-
-                    if (window.GetKey(Keys.Q))
-                    {
-                        var rotation = triangle.Transform.Rotation;
-                        rotation.z += 2 * MathF.PI * fixedDeltaTime;
-                        triangle.Transform.Rotation = rotation;
-                    }
-
-                    if (window.GetKey(Keys.E))
-                    {
-                        var rotation = triangle.Transform.Rotation;
-                        rotation.z -= 2 * MathF.PI * fixedDeltaTime;
-                        triangle.Transform.Rotation = rotation;
-                    }
-
-                    walkDirection = walkDirection.Normalize();
-                    triangle.Transform.Position += walkDirection * movementSpeed * fixedDeltaTime;
-                    float facing = Vector.Dot((rectangle.GetCenter() - triangle.GetCenter()).Normalize(), triangle.Transform.Forward);
-                    float dotProduct = Vector.Dot((circle.GetCenter() - triangle.GetCenter()).Normalize(), triangle.Transform.Forward);
-                    float angle = MathF.Acos(dotProduct);
-                    float factor = angle / MathF.PI;
-                    circle.SetColor(new Color(factor, factor, factor, 1));
-                    bool isTriangleFacing = facing > 0;
-
-                    if (isTriangleFacing)
-                    {
-                        rectangle.SetColor(Color.Red);
-                    }
-                    else
-                    {
-                        rectangle.SetColor(Color.Blue);
-                    }
+                    physics.Update(fixedDeltaTime);
                 }
                 window.Render();
             }
--- Shape.cs
+++ Shape.cs
@@ -1,83 +1,102 @@
-using System;
 using System.Runtime.InteropServices;
-using OpenGL;
 using static OpenGL.Gl;
 
-
-namespace SharpEngine
+namespace SharpEngine 
 {
-    public class Shape
-    {
-        Vertex[] vertices;
-        uint vertexArray;
-        uint vertexBuffer;
-        public Material material;
-        
-        public Transform Transform { get; }
-        
-        public Shape(Vertex[] vertices, Material material)
-        {
-            this.vertices = vertices;
-            this.material = material;
-            LoadTriangleIntoBuffer();
-            Transform = new Transform();
-        }
-        
-        public Vector GetMinBounds()
-        {
-            var min = Transform.Matrix * vertices[0].position;
-            for (var i = 1; i < vertices.Length; i++) 
-            {
-                min = Vector.Min(min, Transform.Matrix * vertices[i].position);
-            }
-            return min;
-        }
-        public Vector GetMaxBounds()
-        {
-            var max = Transform.Matrix * vertices[0].position;
-            for (var i = 1; i < vertices.Length; i++) 
-            {
-                max = Vector.Max(max, Transform.Matrix * vertices[i].position);
-            }
-            return max;
-        }
-        public Vector GetCenter()
-        {
-            return (GetMinBounds() + GetMaxBounds()) / 2;
-        }
-       
-        public unsafe void Render() 
-        {
-            material.Use();
-            material.SetTransform(Transform.Matrix);
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            fixed (Vertex* vertex = &vertices[0]) {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_DYNAMIC_DRAW);
-            }
-            glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
-            glBindVertexArray(0);
-        }
-        public unsafe uint LoadTriangleIntoBuffer()
-        {
-            vertexArray = glGenVertexArray();
-            vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
-            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
-            glEnableVertexAttribArray(0);
-            glEnableVertexAttribArray(1);
-            glBindVertexArray(0);
-            return vertexArray;
-        }
-        public void SetColor(Color color)
-        {
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                vertices[i].color = color;
-            }
-        }
+	public class Shape 
+	{
+            
+		Vertex[] vertices;
+		uint vertexArray;
+		uint vertexBuffer;
 
-    }
+		public Transform Transform { get; }
+		public Material material;
+
+		float mass = 1;
+		float massInverse = 1;
+		public float Mass 
+		{
+			get => this.mass;
+			set 
+			{
+				this.mass = value;
+				this.massInverse = float.IsPositiveInfinity(value) ? 0f : 1f / value;
+			}
+		}
+		public float MassInverse => massInverse;
+		
+		public float gravityScale = 1f;
+		public Vector velocity; // momentum = product of velocity and mass
+		public Vector linearForce;
+		
+		public Shape(Vertex[] vertices, Material material) 
+		{
+			this.vertices = vertices;
+			this.material = material;
+			LoadTriangleIntoBuffer();
+			this.Transform = new Transform();
+		}
+
+		public void SetColor(Color color) 
+		{
+			for (int i = 0; i < this.vertices.Length; i++) 
+			{
+				vertices[i].color = color;
+			}
+		}
+		
+		 void LoadTriangleIntoBuffer() 
+		 {
+			vertexArray = glGenVertexArray();
+			vertexBuffer = glGenBuffer();
+			glBindVertexArray(vertexArray);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+			glVertexAttribPointer(0, 3, GL_FLOAT, false, Marshal.SizeOf<Vertex>(), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
+			glVertexAttribPointer(1, 4, GL_FLOAT, false, Marshal.SizeOf<Vertex>(), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
+			glEnableVertexAttribArray(0);
+			glEnableVertexAttribArray(1);
+			glBindVertexArray(0);
+		}
+
+		public Vector GetMinBounds() 
+		{
+			var min = this.Transform.Matrix * this.vertices[0].position;
+			for (var i = 1; i < this.vertices.Length; i++) 
+			{
+				min = Vector.Min(min, this.Transform.Matrix * this.vertices[i].position);
+			}
+			return min;
+		}
+            
+		public Vector GetMaxBounds() 
+		{
+			var max = this.Transform.Matrix * this.vertices[0].position;
+			for (var i = 1; i < this.vertices.Length; i++) 
+			{
+				max = Vector.Max(max, this.Transform.Matrix * this.vertices[i].position);
+			}
+
+			return max;
+		}
+
+		public Vector GetCenter() 
+		{
+			return (GetMinBounds() + GetMaxBounds()) / 2;
+		}
+
+		public unsafe void Render() 
+		{
+			this.material.Use();
+			this.material.SetTransform(this.Transform.Matrix);
+			glBindVertexArray(vertexArray);
+			glBindBuffer(GL_ARRAY_BUFFER, this.vertexBuffer);
+			fixed (Vertex* vertex = &this.vertices[0]) 
+			{
+				glBufferData(GL_ARRAY_BUFFER, Marshal.SizeOf<Vertex>() * this.vertices.Length, vertex, GL_DYNAMIC_DRAW);
+			}
+			glDrawArrays(GL_TRIANGLES, 0, this.vertices.Length);
+			glBindVertexArray(0);
+		}
+	}
 }
